ea962b8e66ca35ca69582c548ed6c9eb0cf69127
Kaushal <Kaushalkumar304@gmail.com>
2022-09-28 09:10:28 +0530

    fixing cookie issue

--- dist/utils/sendJwtToken.js
+++ dist/utils/sendJwtToken.js
@@ -6,9 +6,7 @@ const sendJwtToken = (user, res) => {
     const cookieTime = parseInt(process.env.COOKIE_TIME);
     const options = {
         expires: new Date(Date.now() + cookieTime * 24 * 60 * 60 * 1000),
-        httpOnly: false,
         secure: true,
-        SameSite: "none",
     };
     //set token in cookie
     res.status(200)
