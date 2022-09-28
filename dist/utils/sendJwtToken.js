a212391660bc0c1461f09e301cda79f39db6a316
Kaushal <Kaushalkumar304@gmail.com>
2022-09-28 09:24:47 +0530

    enabeling flag SameSite:None

--- dist/utils/sendJwtToken.js
+++ dist/utils/sendJwtToken.js
@@ -7,6 +7,7 @@ const sendJwtToken = (user, res) => {
     const options = {
         expires: new Date(Date.now() + cookieTime * 24 * 60 * 60 * 1000),
         secure: true,
+        SameSite: "None",
     };
     //set token in cookie
     res.status(200)
--- utils/sendJwtToken.ts
+++ utils/sendJwtToken.ts
@@ -7,6 +7,7 @@ const sendJwtToken = (user: IUserDocument, res: Response) => {
     const options = {
         expires: new Date(Date.now() + cookieTime * 24 * 60 * 60 * 1000),
         secure: true,
+        SameSite: "None",
     };
 
     //set token in cookie
